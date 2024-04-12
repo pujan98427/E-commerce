4965f8121d63cfebce86c87bee2bad32a409754c
Poojan-98 <chapagain.pujan@gmail.com>
2024-04-12 07:37:23 +0545

    feat: vue js with tailwind and sass fresh setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-3xl font-bold underline">Hello there</h1>
+  </header>
+
+  <RouterView />
+</template>
