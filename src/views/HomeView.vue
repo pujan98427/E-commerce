b202565409b0e4a73546bbcd62137d526d8486bd
Poojan-98 <chapagain.pujan@gmail.com>
2024-05-02 07:58:43 +0545

    feat: home page secction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ const productStore = useProductStore()
 
 productStore.fill()
 
-const categories = [
+let categories = [
   ...new Map(
     productStore.products
       .filter((product) => product.category)
@@ -21,7 +21,7 @@ const categories = [
       ])
   ).values()
 ]
-
+categories = categories.slice(0, 3)
 const sliceProducts = productStore.products.slice(0, 4)
 
 // https://www.youtube.com/watch?v=cfiN8lCA3RM
@@ -63,10 +63,10 @@ const sliceProducts = productStore.products.slice(0, 4)
           <div class="h-48 w-full bg-white" />
         </div>
         <div class="relative py-32">
-          <h1 class="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+          <h1 class="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-5xl">
             Mid-Season Sale
           </h1>
-          <div class="mt-4 sm:mt-6">
+          <div class="mt-6 sm:mt-10">
             <router-link
               :to="{ name: 'shop' }"
               class="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 font-medium text-white hover:bg-indigo-700"
@@ -92,21 +92,25 @@ const sliceProducts = productStore.products.slice(0, 4)
                   <img
                     :src="collection.imageSrc"
                     :alt="collection.imageAlt"
-                    class="h-full w-full object-cover object-center"
+                    class="h-full w-full object-cover object-center transition-all scale-100 group-hover:scale-110 duration-300"
                   />
                 </div>
                 <div
                   class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"
                 />
               </div>
-              <div class="absolute inset-0 flex items-end rounded-lg p-6">
+              <div
+                class="absolute inset-0 flex items-end justify-center text-center rounded-lg p-6"
+              >
                 <div>
-                  <p aria-hidden="true" class="text-sm text-white">Shop the collection</p>
                   <h3 class="mt-1 font-semibold text-white">
-                    <a :href="collection.href">
+                    <router-link
+                      :to="{ name: 'category', params: { id: collection.id } }"
+                      class="transitio-all ease-in-out duration-300 group-hover:text-indigo-600"
+                    >
                       <span class="absolute inset-0" />
                       {{ collection.name }}
-                    </a>
+                    </router-link>
                   </h3>
                 </div>
               </div>
@@ -118,42 +122,43 @@ const sliceProducts = productStore.products.slice(0, 4)
 
     <section aria-labelledby="trending-heading">
       <div class="mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8 lg:pt-32">
-        <div class="md:flex md:items-center md:justify-between">
-          <h2 id="favorites-heading" class="text-2xl font-bold tracking-tight text-gray-900">
-            Trending Products
-          </h2>
-          <a
-            href="#"
-            class="hidden text-sm font-medium text-indigo-600 hover:text-indigo-500 md:block"
-          >
-            Shop the collection
-            <span aria-hidden="true"> &rarr;</span>
-          </a>
-        </div>
+        <h2
+          id="favorites-heading"
+          class="text-3xl text-center font-bold tracking-tight text-gray-900"
+        >
+          Trending Products
+        </h2>
 
         <div
-          class="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8"
+          class="mt-10 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8"
         >
-          <div v-for="product in sliceProducts" :key="product.id" class="group relative">
+          <div
+            v-for="product in sliceProducts"
+            :key="product.id"
+            class="group relative text-center"
+          >
             <div class="w-full overflow-hidden rounded-md group-hover:opacity-75 lg:h-72 xl:h-80">
               <img
                 :src="product.imageSrc"
                 :alt="product.imageAlt"
-                class="h-full w-full object-cover object-center"
+                class="h-full w-full object-cover object-center transition-all scale-100 group-hover:scale-110 duration-300"
               />
             </div>
-            <h3 class="mt-4 text-sm text-gray-700">
-              <a :href="product.href">
+            <h3 class="mt-4 text-lg font-medium text-gray-900">
+              <router-link
+                :to="{ name: 'details', params: { id: product.id } }"
+                class="transitio-all ease-in-out duration-300 hover:text-indigo-600"
+              >
                 <span class="absolute inset-0" />
                 {{ product.name }}
-              </a>
+              </router-link>
             </h3>
             <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
-            <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
+            <p class="mt-1 text-lg font-semibold text-black">${{ product.price }}</p>
           </div>
         </div>
 
-        <div class="mt-8 text-sm md:hidden">
+        <div class="mt-10 text-sm text-center">
           <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
             Shop the collection
             <span aria-hidden="true"> &rarr;</span>
